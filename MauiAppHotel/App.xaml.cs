a09ff8781622c6461dc8233519444a7d40902f5c
VandethCaetano <caetanovandeth@gmail.com>
2024-10-28 16:34:07 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Width = 600;
+            return window;
+
+        }
+    }
+}
