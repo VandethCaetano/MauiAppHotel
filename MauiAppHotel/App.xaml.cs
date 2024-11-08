6d76ce41921a9d2ed6c2759842afbf7cab68ffad
VandethCaetano <caetanovandeth@gmail.com>
2024-11-08 11:43:51 -0300

    adicionar recursos ao aplicativo

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -6,13 +6,13 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage (new Views.ContratacaoHospedagem());
         }
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
             window.Width = 400;
-            window.Width = 600;
+            window.Height = 600;
             return window;
 
         }
